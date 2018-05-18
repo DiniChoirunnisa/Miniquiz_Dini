a70f71fa4fa20f3cfc256c16062b877330894db4
Dini Choirunnisa <dinichoirunnisaa@gmail.com>
2018-05-18 15:06:48 +0700

    miniquiz2

---
+++ Miniquiz2/src/miniquiz2/Miniquiz2.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniquiz2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GeeksFarm
+ */
+public class Miniquiz2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    int n,j,i;
+    
+    Scanner input= new Scanner (System.in);
+    
+    System.out.print ("Masukkan Skala : ");
+    
+    if (!input.hasNextInt())
+        {
+            System.out.print ("Harus angka! Tidak bisa di proses");
+        } 
+        else
+        {
+        n=input.nextInt();
+        
+        i=1;
+        while (i<=2*n-1) 
+          {
+            j=1;
+            while (j<=n) {
+                if (i==1 || i==n || (j==4 && i<n) || (j==1 && i>=n) || (i==2*n-1))
+                { 
+                    System.out.print ("*");
+                }
+                
+                else {
+                    System.out.print (" ");
+                }
+                j++;
+        }
+             i++;
+        System.out.println();
+        }
+    }
+    
+    }
+    }
+
+    
+    
+
